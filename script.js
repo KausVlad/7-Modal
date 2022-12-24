20af0f98a7eacff565667c7fddb466e85088224a
KausVlad <kausvirtual@gmail.com>
2022-12-25 01:10:40 +0200

    modal

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+console.log(btnsShowModal);
+
+const openModal = function () {
+  console.log('unga bunga');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnsShowModal.length; i++) {
+  btnsShowModal[i].addEventListener('click', openModal);
+}
+// const closeModal = function (select) {
+//   select.addEventListener('click', function () {
+//     modal.classList.add('hidden');
+//     overlay.classList.add('hidden');
+//   });
+// };
+// closeModal(btnCloseModal);
+// closeModal(overlay);
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+// btnCloseModal.addEventListener('click', function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// });
+// overlay.addEventListener('click', function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// });
