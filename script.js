a05428efdb3d1b0b48472d0212ecb077179dfd55
KausVlad <kausvirtual@gmail.com>
2022-12-25 21:20:18 +0200

    added event.key

--- script.js
+++ script.js
@@ -4,12 +4,17 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsShowModal = document.querySelectorAll('.show-modal');
-console.log(btnsShowModal);
+// console.log(btnsShowModal);
 
 const openModal = function () {
   console.log('unga bunga');
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
 };
 
 for (let i = 0; i < btnsShowModal.length; i++) {
@@ -24,13 +29,19 @@ for (let i = 0; i < btnsShowModal.length; i++) {
 // closeModal(btnCloseModal);
 // closeModal(overlay);
 
-const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-};
-
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+  // if (event.key === 'Escape') {
+  //   if (!modal.classList.contains('hidden')) {
+  //     closeModal();
+  //   }
+  // }
+  // event.key === 'Escape' ? closeModal() : null;
+}); // робота з клавіатурою
 
 // btnCloseModal.addEventListener('click', function () {
 //   modal.classList.add('hidden');
